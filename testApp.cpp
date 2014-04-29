5da7a5d99ce5b0e4cdd096f1d53218c2b06cbdc9
Xzalor <quentin.-@hotmail.fr>
2014-04-29 17:48:45 +0200

    Changement dans Classe Grains et test de blocs dans testapp

--- testApp.cpp
+++ testApp.cpp
@@ -1,4 +1,6 @@
 #include "testApp.h"
+#include <stdio.h>
+#include <time.h>
 #include "Module_Chateau\Header\Grains.h"
 //--------------------------------------------------------------
 void testApp::setup() {
@@ -55,49 +57,80 @@ void testApp::draw() {
 	info += "FPS: "+ofToString(ofGetFrameRate(), 1)+"\n";
 	ofSetHexColor(0x444342);
 	ofDrawBitmapString(info, 30, 30);
+	/*
+	for (int longueur = 0; longueur < 20; longueur++){
+		for (int largeur = 0; largeur < 70; largeur++){
+			ofFill();
+			ofSetColor(0, 2 + longueur * 20, 2 + largeur * 20);
+			ofRect(400 + longueur * 2, 720 - largeur * 2, 2, 2);
+		}
+	}*/
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key) {
 	
 	// Tests Claire
-	if (key == 'g'){
+	//if (key == 'g'){
 		//Grains(float density, float size, ofVec2f position)
 		/* essai, ne pas effacer :)
 		ofVec2f v1;
 		v1.set(mouseX, mouseY);
 		Grains g(30.0, 1.0, v1); 
 		*/
-		float r = 10.0;
-		for (int i = 0; i < 5; i++) {
-			grains.push_back(ofPtr<Grains>(new Grains));
-			grains.back().get()->setPhysics(30.0, 0.73, 0.5);
-			grains.back().get()->setup(box2d.getWorld(), mouseX+i, mouseY+i, r);
-		}
-	}
+		//float r = 10.0;
+		//for (int i = 0; i < 5; i++) {
+		//	grains.push_back(ofPtr<Grains>(new Grains(10)));
+		//	grains.back().get()->setPhysics(30.0, 0.73, 0.5);
+		//	grains.back().get()->setup(box2d.getWorld(), mouseX+i, mouseY+i, r);
+	//	}
+	//}
 
 	if(key == 'c') {
-		float r = ofRandom(4, 20);
 		circles.push_back(ofPtr<ofxBox2dCircle>(new ofxBox2dCircle));
-		circles.back().get()->setPhysics(30.0, 0.73, 0.5);
-		circles.back().get()->setup(box2d.getWorld(), mouseX, mouseY, r);
+		circles.back().get()->setPhysics(2, 0.73, 0.5);
+		circles.back().get()->setup(box2d.getWorld(), mouseX, mouseY, 40);
 		
 	}
 
-	if (key == 'b') {		//Tests pour avoir la taille d'un grain et faire des blocs avec une boucle, resultat : 
+	if (key == 'j') {		//Tests pour avoir la taille d'un grain et faire des blocs avec une boucle, resultat : 
 								//Il faut faire des blocs avec ofxBox2dRect avec une texture de grains pour paraitre, et
 								//quand il y a contact le faire devenir un amat de grain, mais sinon tant au niveau des fps et
 								//de la cohésion des blocs ça ne marche pas
 
-		for (int longueur = 0; longueur < 20; longueur++){
-			for (int largeur = 0; largeur < 10; largeur++){
+		
+		for (int hauteur = 0; hauteur < 5; hauteur++){
+			for (int longueur = 0; longueur < 10; longueur++){
 				boxes.push_back(ofPtr<ofxBox2dRect>(new ofxBox2dRect));
-				boxes.back().get()->setPhysics(1.0, 0, 10000000000000000000);
-				boxes.back().get()->setup(box2d.getWorld(), mouseX + longueur*2, mouseY + largeur*2, 2, 2);
+				boxes.back().get()->setPhysics(10.0, 0, 1);
+				boxes.back().get()->setup(box2d.getWorld(), mouseX + longueur * 10, mouseY - hauteur *10 , 10, 10);
+			}
+		}		
+	}
+
+	if (key == 'g') {	
+		ofVec2f mouse;
+		mouse.set(mouseX, mouseY);
+		for (int hauteur = 0; hauteur < 10; hauteur++){
+			for (int longueur = 0; longueur < 30; longueur++){
+				grains.push_back(ofPtr<Grains>(new Grains(mouse)));
+				//grains.back().get()->setup(box2d.getWorld(), mouseX + longueur * 10, mouseY - hauteur * 10, 10, 10);
 			}
 		}
 	}
-	
+
+	if (key == 'h') {
+		for (int hauteur = 0; hauteur < 7; hauteur++){
+			for (int longueur = 0; longueur < 15; longueur++){
+				boxes.push_back(ofPtr<ofxBox2dRect>(new ofxBox2dRect));
+				boxes.back().get()->setPhysics(10.0, 0, 1);
+				boxes.back().get()->setup(box2d.getWorld(), mouseX + longueur * 10, mouseY - hauteur * 10, 10, 10);
+			}
+		}
+	}
+
+
+
 	if(key == 't') ofToggleFullscreen();
 }
 
