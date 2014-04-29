2e5424eee7d6659da52e6badbd88ea8fc258aa12
Anhaflint <anha.flint@gmail.com>
2014-04-29 23:07:15 +0200

    Essais d'optimisation

--- testApp.cpp
+++ testApp.cpp
@@ -31,10 +31,18 @@ void testApp::draw() {
 		circles[i].get()->draw();
 	}
 	
+	//Creation d'un rectangle
+	ofFill();
+	ofSetColor(0, 255, 255);
+	ofRect(200,520, 100, 200);
+
+
 	for(int i=0; i<boxes.size(); i++) {
 		ofFill();
 		ofSetHexColor(0xBF2545);
 		boxes[i].get()->draw();
+
+		printf("\n %d", !boxes[i].get()->isBody());
 	}
 
 	// Ajouté par Claire -----------------------------------------
@@ -42,6 +50,16 @@ void testApp::draw() {
 		ofFill();
 		ofSetHexColor(0xBF2545);
 		grains[i].get()->draw();
+
+	}
+
+
+	for (int longueur = 0; longueur < 20; longueur++){
+		for (int largeur = 0; largeur < 70; largeur++){
+			ofFill();
+			ofSetColor(0, 2+longueur*20, 2+largeur*20);
+			ofRect(400 + longueur * 2, 720 - largeur * 2, 2, 2);
+		}
 	}
 	// draw the ground
 	box2d.drawGround();
@@ -59,46 +77,71 @@ void testApp::draw() {
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key) {
-	
-	// Tests Claire
-	if (key == 'g'){
-		//Grains(float density, float size, ofVec2f position)
-		/* essai, ne pas effacer :)
-		ofVec2f v1;
-		v1.set(mouseX, mouseY);
-		Grains g(30.0, 1.0, v1); 
-		*/
-		float r = 10.0;
-		for (int i = 0; i < 5; i++) {
-			grains.push_back(ofPtr<Grains>(new Grains));
-			grains.back().get()->setPhysics(30.0, 0.73, 0.5);
-			grains.back().get()->setup(box2d.getWorld(), mouseX+i, mouseY+i, r);
-		}
-	}
 
-	if(key == 'c') {
+	// Tests Claire
+	//if (key == 'g'){
+	//Grains(float density, float size, ofVec2f position)
+	/* essai, ne pas effacer :)
+	ofVec2f v1;
+	v1.set(mouseX, mouseY);
+	Grains g(30.0, 1.0, v1);
+	*/
+	//float r = 10.0;
+	//for (int i = 0; i < 5; i++) {
+	//grains.push_back(ofPtr<Grains>(new Grains));
+	//grains.back().get()->setPhysics(30.0, 0.73, 0.5);
+	//grains.back().get()->setup(box2d.getWorld(), mouseX+i, mouseY+i, r);
+	// }
+	//}
+
+	if (key == 'c') {
 		float r = ofRandom(4, 20);
 		circles.push_back(ofPtr<ofxBox2dCircle>(new ofxBox2dCircle));
 		circles.back().get()->setPhysics(30.0, 0.73, 0.5);
 		circles.back().get()->setup(box2d.getWorld(), mouseX, mouseY, r);
-		
+		circles.back().get()->bodyDef.active = false;
+		if (circles.back().get()->bodyDef.active == false) printf("lalaala faux");
+		else printf("choucroute\n");
 	}
 
-	if (key == 'b') {		//Tests pour avoir la taille d'un grain et faire des blocs avec une boucle, resultat : 
-								//Il faut faire des blocs avec ofxBox2dRect avec une texture de grains pour paraitre, et
-								//quand il y a contact le faire devenir un amat de grain, mais sinon tant au niveau des fps et
-								//de la cohésion des blocs ça ne marche pas
+	if (key == 'j') {  //Tests pour avoir la taille d'un grain et faire des blocs avec une boucle, resultat : 
+		//Il faut faire des blocs avec ofxBox2dRect avec une texture de grains pour paraitre, et
+		//quand il y a contact le faire devenir un amat de grain, mais sinon tant au niveau des fps et
+		//de la cohésion des blocs ça ne marche pas
 
-		for (int longueur = 0; longueur < 20; longueur++){
-			for (int largeur = 0; largeur < 10; largeur++){
+
+		for (int hauteur = 0; hauteur < 5; hauteur++){
+			for (int longueur = 0; longueur < 10; longueur++){
 				boxes.push_back(ofPtr<ofxBox2dRect>(new ofxBox2dRect));
-				boxes.back().get()->setPhysics(1.0, 0, 10000000000000000000);
-				boxes.back().get()->setup(box2d.getWorld(), mouseX + longueur*2, mouseY + largeur*2, 2, 2);
+				boxes.back().get()->setPhysics(10.0, 0, 1);
+				boxes.back().get()->setup(box2d.getWorld(), mouseX + longueur * 10, mouseY - hauteur * 10, 10, 10);
 			}
 		}
 	}
-	
-	if(key == 't') ofToggleFullscreen();
+
+	if (key == 'g') {
+		for (int hauteur = 0; hauteur < 10; hauteur++){
+			for (int longueur = 0; longueur < 30; longueur++){
+				boxes.push_back(ofPtr<ofxBox2dRect>(new ofxBox2dRect));
+				boxes.back().get()->setPhysics(10.0, 0, 1);
+				boxes.back().get()->setup(box2d.getWorld(), mouseX + longueur * 10, mouseY - hauteur * 10, 10, 10);
+
+			}
+		}
+	}
+
+	if (key == 'h') {
+		for (int hauteur = 0; hauteur < 7; hauteur++){
+			for (int longueur = 0; longueur < 15; longueur++){
+				boxes.push_back(ofPtr<ofxBox2dRect>(new ofxBox2dRect));
+				boxes.back().get()->setPhysics(10.0, 0, 1);
+				boxes.back().get()->setup(box2d.getWorld(), mouseX + longueur * 10, mouseY - hauteur * 10, 10, 10);
+				boxes.back().get()->bodyDef.active = false;
+			}
+		}
+
+		if (key == 't') ofToggleFullscreen();
+	}
 }
 
 //--------------------------------------------------------------
