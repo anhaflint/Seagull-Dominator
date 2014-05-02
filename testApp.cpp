438f3f1c29f3aa973478e6f7dc52cc70068594b5
Anhaflint <anha.flint@gmail.com>
2014-05-02 20:16:26 +0200

    Utilisation des blocs

--- testApp.cpp
+++ testApp.cpp
@@ -26,40 +26,18 @@ void testApp::update() {
 //--------------------------------------------------------------
 void testApp::draw() {
 	
-	for(int i=0; i<circles.size(); i++) {
+	/* Dessin d'un bloc
+	 */
+	if (bloc != NULL)
+		bloc->draw();
+
+	
+
+	for (int i = 0; i<circles.size(); i++) {
 		ofFill();
 		ofSetHexColor(0xf6c738);
-		circles[i].get()->draw();
+		circles[i]->draw();
 	}
-	
-	/*
-	for(int i=0; i<boxes.size(); i++) {
-		ofFill();
-		ofSetHexColor(0xBF2545);
-		boxes[i].get()->draw();
-
-		printf("\n %d", !boxes[i].get()->isBody());
-	}
-	*/
-	// Ajouté par Claire -----------------------------------------
-	
-	for (int i = 0; i < grains.size(); i++) {
-		ofFill();
-		ofSetHexColor(0xBF2545);
-		grains[i].get()->draw();
-
-	}
-	
-	
-	/* exemple de dessin d'un rectangle avec OF
-	for (int longueur = 0; longueur < 20; longueur++){
-		for (int largeur = 0; largeur < 70; largeur++){
-			ofFill();
-			ofSetColor(0, 2+longueur*20, 2+largeur*20);
-			ofRect(400 + longueur * 2, 720 - largeur * 2, 2, 2);
-		}
-	}
-	*/
 	// draw the ground
 	box2d.drawGround();
 	
@@ -79,48 +57,7 @@ void testApp::draw() {
 void testApp::keyPressed(int key) {
 
 	// Tests Claire
-	/* Tests Grains
-	*/
-	if (key == 'l'){
-		float r = 10.0;
-		for (int i = 0; i < 5; i++) {
-			grains.push_back(ofPtr<Grains>(new Grains));
-			grains.back().get()->setPhysics(30.0, 0.73, 0.5);
-			grains.back().get()->ofxBox2dRect::setup(box2d.getWorld(), mouseX + i, mouseY + i, r, r);
-		}
-	}
-	
-	if (key == 'j') {
-		for (int hauteur = 0; hauteur < 5; hauteur++){
-			for (int longueur = 0; longueur < 10; longueur++){
-				grains.push_back(ofPtr<Grains>(new Grains));
-				grains.back().get()->setPhysics(10.0, 0, 1);
-				grains.back().get()->setup(box2d.getWorld(), mouseX + longueur * 10, mouseY - hauteur * 10, 10, 10);
-			}
-		}
-	}
-	
-	if (key == 'g') {
-		for (int hauteur = 0; hauteur < 10; hauteur++){
-			for (int longueur = 0; longueur < 30; longueur++){
-				grains.push_back(ofPtr<Grains>(new Grains));
-				grains.back().get()->setPhysics(10.0, 0, 1);
-				grains.back().get()->setup(box2d.getWorld(), mouseX + longueur * 10, mouseY - hauteur * 10, 10, 10);
-			}
-		}
-	}
-	
-
-	if (key == 'h') {
-		for (int hauteur = 0; hauteur < 7; hauteur++){
-			for (int longueur = 0; longueur < 15; longueur++){
-				grains.push_back(ofPtr<Grains>(new Grains));
-				grains.back().get()->setPhysics(10.0, 0, 1);
-				grains.back().get()->setup(box2d.getWorld(), mouseX + longueur * 10, mouseY - hauteur * 10, 10, 10);
-			}
-		}
-	}
-
+	bloc = ofPtr<Bloc>(new Bloc(key, box2d, mouseX, mouseY));
 
 	if (key == 'c') {
 		circles.push_back(ofPtr<ofxBox2dCircle>(new ofxBox2dCircle));
--- testApp.h
+++ testApp.h
@@ -22,9 +22,9 @@ public:
 	void mouseReleased(int x, int y, int button);
 	void resized(int w, int h);
 	
-	ofxBox2d                            box2d;			  //	the box2d world
-	// vector    <ofPtr<ofxBox2dCircle> >	grains;
-	vector <ofPtr<Grains>> grains;
+	ofxBox2d  box2d;			  //	the box2d world
+	ofPtr<Bloc> bloc;
+	
 	vector    <ofPtr<ofxBox2dCircle> >	circles;		//	default box2d circles
 	vector	  <ofPtr<ofxBox2dRect> >	boxes;			  //	defalut box2d rects
 	b2WeldJointDef weldJointDef;
