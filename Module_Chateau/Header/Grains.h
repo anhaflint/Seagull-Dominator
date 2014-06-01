c5061091f214e0e9ef53597e6903dd77fd51daf0
Anhaflint <anha.flint@gmail.com>
2014-06-02 00:57:42 +0200

    Documentation de la classe Grains

--- Module_Chateau/Header/Grains.h
+++ Module_Chateau/Header/Grains.h
@@ -1,23 +1,49 @@
 #ifndef _GRAINS_H
 #define _GRAINS_H
 
+/*!
+ * \file Grains.h
+ * \brief Classe représentant un grain d'un bloc
+ * \author Claire REMY
+ */
+
 #include "ofxBox2d.h"
 #include "ofMain.h"
 #include "ofxBox2dRect.h"
 #include "Util\Header\Timer.h"
 #include "Module_Chateau\Header\ObjetPhysique.h"
 
+/*!
+ * \class
+ * \brief Classe représentant un grain d'un bloc
+ * hérite de la classe ofxBox2dRect
+ */
 class Grains : public ObjetPhysique, public ofxBox2dRect {
 protected:
-	Timer timer;
-	ofVec2f prec;
-	ofVec2f current;	
+	Timer timer; /*!< Timer permettant de décompter le temps jusqu'à la disparition d'un grain */
+	ofVec2f current; /*!< Position courante du grain, fonctionnalité pas encore utilisée */
 public:
-	static const int size = 5;
+	static const int size = 5; /*!< taille d'un grain */
+
+	/*! 
+	 * \brief Constructeur par défaut de la classe grain
+	 */
 	Grains();
-	Grains(ofVec2f); // Modifié le type de Position vers ofVec2f
-	int deplacement(ofVec2f courant, ofVec2f precedent); // Modifié le type de position vers ofVec2f
+
+	/*!
+	 * \brief Constructeur de la classe Grain permettant de stocker la position
+	 * Cette Fonctionnalité n'est pas encore utilisée
+	 */
+	Grains(ofVec2f);
+
+	/*!
+	 * \brief Fonction permettant la disparition d'un grain après un certain temps
+	 */
 	void disparition();
+
+	/*!
+	 * \brief Fonction permettant de dessiner un grain à l'écran
+	 */
 	virtual void draw();
 };
 
