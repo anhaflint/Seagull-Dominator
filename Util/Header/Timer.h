feb8564607814c45a7c4db1d41b21fefdd414874
Anhaflint <anha.flint@gmail.com>
2014-06-02 14:23:09 +0200

    documentation timer

--- Util/Header/Timer.h
+++ Util/Header/Timer.h
@@ -1,15 +1,30 @@
 #pragma once
-
+/*!
+ * \file Timer.h
+ * \brief Classe permettant de gerer le temps
+ * \author Quentin GROS, Mounia HAMEDI (relecture)
+ */
 #include <time.h>
 
 class Timer {
 public:
+	/*!
+	 * \brief Constructeur
+	 */
 	Timer() { start = ::time(NULL); }
+
+	/*!
+	 * \brief Destructeur
+	 */
 	virtual ~Timer() {}
 
+	/*!
+	 * \brief Fonction permettant de calculer le temps elapsé
+	 */
 	unsigned long Lapse() {
 		return ::time(NULL) - start;
 	}
+
 private:
-	unsigned long start;
+	unsigned long start; /*!< Temps de depart */
 };
