5eb2d4806cc552ce7e5c5519bd9df532dbd20d88
Anhaflint <anha.flint@gmail.com>
2014-06-02 14:19:58 +0200

    Documentation functions

--- Util/Header/functions.h
+++ Util/Header/functions.h
@@ -1,6 +1,18 @@
+#pragma once
+
+/*!
+ * \file functions.h
+ * \brief Fichier contenant les fonctions utiles au developpement
+ * \author Claire REMY, Mounia HAMEDI (relecture)
+ */
+
 #include <direct.h>
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
 
+/*!
+ * \brief Fonction retournant le chemin absolu vers un nom de fichier a partir du repertoire courant
+ * \param file Nom du fichier a recuperer
+ */
 char* getAbsolutePath(char* file);
